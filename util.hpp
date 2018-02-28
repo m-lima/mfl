ee0e32669d34b5217018fd02748daca420639f19
Marcelo Lima <marcelo@telenordigital.com>
2018-02-28 21:07:40 +0100

    Better multiplication for compile speed

--- util.hpp
+++ util.hpp
@@ -24,7 +24,7 @@ namespace mfl {
 
     template <typename T, typename E>
     constexpr T safeCast(E value) {
-        return static_cast<T>(value & ~(1 << (sizeof(T) * 8 - 1)));
+        return static_cast<T>(value & ~(1 << ((sizeof(T) << 3) - 1)));
     }
   }
 }
